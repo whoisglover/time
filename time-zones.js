45fa8ae6949d471013abaaeae425e2b88c349026
Danny Glover <danny@plastiq.com>
2020-04-25 15:50:36 -0700

    checking env variable for port for hosting

--- time-zones.js
+++ time-zones.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const moment = require('moment-timezone')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.get('/time', (req, res) => {
   console.log('in the /time request')
