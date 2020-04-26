2d62147a91a65fa681ecd93ad53b252549a52d18
Danny Glover <danny@plastiq.com>
2020-04-25 17:57:54 -0700

    adding swagger documentation at /docs

--- time-zones.js
+++ time-zones.js
@@ -1,8 +1,44 @@
 const express = require('express')
 const moment = require('moment-timezone')
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
 const app = express()
 const port = process.env.PORT || 3000
 
+
+
+/**
+ * @swagger
+ * path:
+ *  /time:
+ *    get:
+ *      summary: Fetch the wall clock time of the server
+ *      tags: [Time]
+ *      parameters:
+ *        - in: query
+ *          name: zone
+ *          schema:
+ *            type: string
+ *            example: America/New_York
+ *          required: false
+ *          description: Timezone to return the time in
+ *        - in: query
+ *          name: strict
+ *          schema:
+ *            type: boolean
+ *          required: false
+ *          description: Strict Mode - if enabled and Time Zone is provided, the zone must be valid
+ *      responses:
+ *        "200":
+ *          description: OK
+ *          examples:
+ *            application/json: {"time":"23:23:11"}
+ *        "400":
+ *          description: Strict mode enabled and invalid Time Zone provided
+ *
+ *
+ */
 app.get('/time', (req, res) => {
   console.log('in the /time request')
 
@@ -25,6 +61,20 @@ app.get('/time', (req, res) => {
   }
 })
 
+
+
+/**
+ * @swagger
+ * path:
+ *  /time-zones:
+ *    get:
+ *      summary: Fetch an array of time zones
+ *      tags: [Time]
+ *      responses:
+ *        "200":
+ *          description: OK
+ *
+ */
 app.get('/time-zones', (req, res) => {
 
   const zones = moment.tz.names()
@@ -33,5 +83,45 @@ app.get('/time-zones', (req, res) => {
 
   res.send(zones)
 })
+
+
+// Swagger set up
+const options = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Danny's Time API",
+      version: "1.0.0",
+      description:
+        "A simple project to return the wall clock time of server.",
+      license: {
+        name: "MIT",
+        url: "https://choosealicense.com/licenses/mit/"
+      },
+      contact: {
+        name: "Danny Glover",
+        url: "http://github.com/whoisglover",
+        email: "djglover00@gmail.com"
+      }
+    },
+    servers: [
+      {
+        url: "http://localhost:3000"
+      },
+      {
+        url: "https://immense-oasis-84897.herokuapp.com/"
+      },
+    ]
+  },
+  apis: ["./time-zones.js"]
+};
+const specs = swaggerJsdoc(options);
+app.use("/docs", swaggerUi.serve);
+app.get(
+  "/docs",
+  swaggerUi.setup(specs, {
+    explorer: true
+  })
+);
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
